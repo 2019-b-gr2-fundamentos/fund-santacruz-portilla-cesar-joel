d9afffcd233354ec244c8741113e624da61c0e5a
CesarSant2000 <cesar.santacruz@epn.edu.ec>
2020-01-09 17:54:36 -0500

    text

--- 06-proyectos/index.js
+++ 06-proyectos/index.js
@@ -13,3 +13,6 @@ console.log(resultado);
 var multiplicacionLibrerias = require('libreria-cssu/lib/multiplicacion');
 var resultado = multiplicacionLibrerias(1, 2);
 console.log(resultado);
+
+var PI = require('libreria-cssu/lib/pi')
+console.log('la constante pi es', PI);
