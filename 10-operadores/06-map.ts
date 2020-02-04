6042b8f8e07b0fb86fb41f367bf1679b024cc2db
CesarSant2000 <cesar.santacruz@epn.edu.ec>
2020-02-03 22:17:11 -0500

    update 03/02/2020

--- 10-operadores/06-map.ts
+++ 10-operadores/06-map.ts
@@ -1,13 +1,15 @@
 
-export function map(arreglo: any[],
+export function map(arregloOriginal: any[],
     funcion: (valorActual:any, indice?:number,arreglo?:any[]) => any[]): any{
         let funMap = [];
+        const arreglo = [...arregloOriginal];
         for( let i = 0; i < arreglo.length; i++){
         const respuestaFuncion = funcion(
             arreglo[i],
             i,
-            arreglo,
+            [...arreglo],
         );
+        funMap.push(respuestaFuncion)
     }
-
+    return funMap
 }
